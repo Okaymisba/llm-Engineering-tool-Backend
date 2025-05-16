0dad19d742d80158e900b783a3346c47e845918b
okaymisba <msbahsarfaraz@gmail.com>
2025-05-16 11:47:00 +0500

    Refactor image OCR and object detection logic.
    Removed `parse_image` function and integrated image processing into `extract_image_data.py` using YOLO and PyTesseract. Simplified `chat` endpoint to handle uploaded images with the updated `extract_image_data`.

--- routers/chat.py
+++ routers/chat.py
@@ -3,6 +3,7 @@ from typing import Optional, Annotated
 from fastapi import APIRouter, File, UploadFile, Depends
 from pydantic import BaseModel
 
+from functions.extract_image_data.extract_image_data import extract_image_data
 from models.user import User
 from routers.auth import get_current_user
 
@@ -14,18 +15,20 @@ class Chat(BaseModel):
     model: str
     image_processing_algo: str
     document_semantic_search: str
-    upload_image: Optional[list[UploadFile]] = File(None)
-    upload_document: Optional[list[UploadFile]] = File(None)
 
 
 @router.get("/chat")
-async def chat(data: Chat, current_user: Annotated[User, Depends(get_current_user)]):
+async def chat(data: Chat,
+               current_user: Annotated[User, Depends(get_current_user)],
+               upload_image: Optional[list[UploadFile]] = File(None),
+               upload_document: Optional[list[UploadFile]] = File(None),
+               ):
     image_data = []
     document_data = []
 
     if data.upload_image:
         for image in data.upload_image:
-            return image
+            image_data.append(extract_image_data(image.file))
     if data.upload_document:
         for document in data.upload_document:
             return document
